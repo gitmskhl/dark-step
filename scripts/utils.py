81a6af6735140ebb29aff19f1271247a8ee624dc
gitmskhl <gglmskhl@gmail.com>
2026-04-04 00:16:40 +0300

    add Animation class

---
+++ scripts/utils.py
@@ -0,0 +1,30 @@
+import pygame
+
+def load_image(path: str, scale: float, colorkey=None) -> pygame.Surface:
+    """Load an image from the specified path, scale it, and set a colorkey if provided."""
+    image = pygame.image.load(path).convert_alpha()
+    if scale != 1.0:
+        width = int(image.get_width() * scale)
+        height = int(image.get_height() * scale)
+        image = pygame.transform.scale(image, (width, height))
+    if colorkey is not None:
+        image.set_colorkey(colorkey)
+    
+    return image
+
+
+def load_images(path: str, scale: float, count: int, colorkey=None) -> list[pygame.Surface]:
+    """Load multiple images from the specified path with a numbered format, scale them, and set a colorkey if provided."""
+    spritesheet = pygame.image.load(path).convert_alpha()
+    images = []
+    for i in range(count):
+        image = spritesheet.subsurface((i * spritesheet.get_width() // count, 0, spritesheet.get_width() // count, spritesheet.get_height()))
+        if scale != 1.0:
+            width = int(image.get_width() * scale)
+            height = int(image.get_height() * scale)
+            image = pygame.transform.scale(image, (width, height))
+        if colorkey is not None:
+            image.set_colorkey(colorkey)
+        images.append(image)
+    return images
+
